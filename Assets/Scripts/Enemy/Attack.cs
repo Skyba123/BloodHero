6729e657cfd5b6421c5606e1f408067c8c07ff97
Skyba123 <skyba.serhi@gmail.com>
2023-11-19 20:10:22 +0200

    Version 0.1.2
    Finished with attack by enemy

--- Assets/Scripts/Enemy/Attack.cs
+++ Assets/Scripts/Enemy/Attack.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
@@ -14,6 +15,7 @@ public class Attack : MonoBehaviour
 
     private Animator anim;
 
+    private Health playerHealth;
     private void Awake()
     {
         anim = GetComponent<Animator>();
@@ -38,6 +40,11 @@ public class Attack : MonoBehaviour
            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
              0, Vector2.left, 0, playerLayer);
 
+        if(hit.collider != null)
+        {
+            playerHealth = hit.collider.GetComponent<Health>();
+        }
+
         return hit.collider != null;
     }
     private void OnDrawGizmos()
@@ -45,5 +52,13 @@ public class Attack : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+        {
+            playerHealth.TakeDamage(damage);
+        }
     }
 }
