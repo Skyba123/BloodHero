bce138813042bdcc8a494f5a4dda829ef03c3d0b
Say my name <kowalsan4ik@gmail.com>
2024-01-06 22:24:20 +0200

    Player stays still after death Added 'if != null' to prevent NullReferenceException

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,17 +8,18 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int maxHealth;
 
     [SerializeField] private Animator animator;
-
+    
     [SerializeField] private GameObject enemyPatrol;
     
     private int currentHealth;
 
-    private Rigidbody2D rb2d;
+    private Rigidbody2D rigidBody2D;
+    
     private void Start()
     {
         currentHealth = maxHealth;
         
-        rb2d = GetComponent<Rigidbody2D>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
     }
     
     public void TakeDamage(int damage)
@@ -37,8 +38,12 @@ public class Enemy : MonoBehaviour
     {
         animator.SetBool("isDead", true);
         GetComponent<Collider2D>().enabled = false;
-        rb2d.bodyType = RigidbodyType2D.Static;
-        enemyPatrol.GetComponent<EnemyPatrol>().Death();
+        rigidBody2D.bodyType = RigidbodyType2D.Static;
+        
+        if (enemyPatrol != null)
+        {
+            enemyPatrol.GetComponent<EnemyPatrol>().Death();
+        }
     }
     
     
