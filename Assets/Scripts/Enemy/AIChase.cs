e1d4802a170bf04ba3f12db9bcd9cc1f82913040
Skyba123 <skyba.serhi@gmail.com>
2024-01-07 06:06:33 +0200

    Update AIChase.cs

--- Assets/Scripts/Enemy/AIChase.cs
+++ Assets/Scripts/Enemy/AIChase.cs
@@ -27,8 +27,6 @@ public class AIChase : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
 
-
-
         if (distance < 8)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
