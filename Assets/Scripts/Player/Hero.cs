393f531bdb55ebb0d2bc0d4451e4c183264a1c68
BirinVlad <90749149+BirinVlad@users.noreply.github.com>
2024-01-11 16:58:38 +0200

    updata

--- Assets/Scripts/Player/Hero.cs
+++ Assets/Scripts/Player/Hero.cs
@@ -13,6 +13,7 @@ public class Hero : Sounds
     
     [SerializeField] private float jumpForce;
     
+    
     private Rigidbody2D body;
     private Animator anim;
     private bool grounded;
@@ -29,18 +30,17 @@ public class Hero : Sounds
         //Flip player when moving left-right
         if (horizontalInput > 0.01)
         {
-            AudioManager.instance.Play("Mooving");
-            transform.localScale = new Vector3(4, 4, 1);
             
+            transform.localScale = new Vector3(4, 4, 1);
         }
         else if (horizontalInput < -0.01f)
         {
-            AudioManager.instance.Play("Mooving");
+            
             transform.localScale = new Vector3(-4, 4, 1);
             
         }
 
-        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        if (Input.GetKey(KeyCode.W) && grounded)
             Jump();
 
         //Set animator
